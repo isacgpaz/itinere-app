254eb5685403ba87ff0cade4fa60acf52999e055
Isac Paz <isacgomesp@gmail.com>
2024-09-03 14:28:11 -0300

    feat: add suspense provider to home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,11 @@
 import { CollaboratorsBanner } from "@/components/collaborators-banner";
 import { Travels } from "@/components/travels";
 import { HeaderSearch } from "./components/header-search";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <>
+    <Suspense>
       <HeaderSearch />
 
       <main className="flex flex-col gap-6 my-12 max-w-lg mx-auto">
@@ -14,6 +15,6 @@ export default function Home() {
 
         <CollaboratorsBanner className="mx-8 sm:mx-0" />
       </main>
-    </>
+    </Suspense>
   );
 }
